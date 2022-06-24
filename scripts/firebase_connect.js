7fa79cd5ebc1d27785d3ae1ed758768fbd3318d8
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2022-06-25 00:13:17 +0530

    Firebase-hosting

---
+++ scripts/firebase_connect.js
@@ -0,0 +1,22 @@
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1VhQ3qEY1tB4_myH8CYjUlxyzUgnMb8w",
+    authDomain: "cuts4-4ebba.firebaseapp.com",
+    databaseURL: "https://cuts4-4ebba-default-rtdb.firebaseio.com",
+    projectId: "cuts4-4ebba",
+    storageBucket: "cuts4-4ebba.appspot.com",
+    messagingSenderId: "828942630848",
+    appId: "1:828942630848:web:026d8f33d4e976e8a9d225"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  
