ff4f2ebfffa601f00b5a15b13c96cee9ac3e4583
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2022-06-25 22:24:11 +0530

    signout

--- public/scripts/firebase_auth.js
+++ public/scripts/firebase_auth.js
@@ -2,20 +2,19 @@ import {app} from "./firestore_connect.js";
 
 
 const provider = new GoogleAuthProvider(app);
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
 
 const auth = getAuth(app);
-function redirect()
-{
-  window.location.replace("./recharge.html");
-}
+
+
+
  signInWithPopup(auth, provider)
   .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     // The signed-in user info
-    redirect();
+    window.location.replace("./recharge.html");
    //window.location = "./recharge.html";
     const user = result.user;
     alert("Logged in");
@@ -30,6 +29,17 @@ function redirect()
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
-  var c=document.getElementById("SubmitLogin");
+  var c=document.getElementById("GoogleSignIn");
   c.addEventListener('click',function(){signInWithPopup(auth,provider)});
-  
+
+function signoutSite()
+{
+  signOut(auth).then(() => {
+  window.location.replace("./signin.html");
+}).catch((error) => {
+ alert("error");
+});
+}
+
+var c2=document.getElementById("");
+c2.addEventListener('click',signoutSite);
