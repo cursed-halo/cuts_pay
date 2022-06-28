523699ed2412009f7a635db983878f318a1f20a9
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2022-06-28 19:46:58 +0530

    small changes to sign in sign out

--- public/scripts/signOut.js
+++ public/scripts/signOut.js
@@ -2,13 +2,21 @@ import {auth,provider} from "./firebase_auth.js"
 import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
 function signoutSite()
 {
+  var user=localStorage.getItem("user");
+  if(user=="null")
+  {
+    alert("You are not yet signed in");
+    return;
+  }
   signOut(auth).then((e) => {
-    alert(auth.currentUser);
+    
     localStorage.setItem("user","null")
+    alert("you have successfully signed out")
   window.location.replace("./index.html");
 }).catch((error) => {
  alert("error");
 });
 }
-var c2=document.getElementById("googleSignOut");
+
+var c2=document.getElementById("googleSignOutRedux");
 c2.addEventListener('click',signoutSite);
