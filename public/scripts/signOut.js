c70e02497d91491b6cbe01951049b40eb11963c2
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2022-06-28 21:07:08 +0530

    .

--- public/scripts/signOut.js
+++ public/scripts/signOut.js
@@ -3,15 +3,15 @@ import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "https://ww
 function signoutSite()
 {
   var user=localStorage.getItem("user");
-  if(user==null)
+  if(user=="null")
   {
     alert("You are not yet signed in");
     return;
   }
   signOut(auth).then((e) => {
     
-    localStorage.setItem("user",null)
-    alert("you have successfully signed out")
+    localStorage.setItem("user","null")
+    alert("you are successfully signed")
   window.location.replace("./index.html");
 }).catch((error) => {
  alert("error");
