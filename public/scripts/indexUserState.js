8a5fff9355cf2a9a9c62a3eb255ce9b6fe7a4167
abhip2565 <paul.apaul.abhishek.ap@gmail.com>
2022-06-29 17:47:24 +0530

    started with register page

--- public/scripts/indexUserState.js
+++ public/scripts/indexUserState.js
@@ -1,5 +1,10 @@
 var userEmail=localStorage.getItem("user")
 
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js"; 
+import {db} from "./firestore_connect.js"
+import {  query, where,getDocs } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+import {auth,provider} from "./firebase_auth.js"
+import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
 if(userEmail!="null")
 {
     console.log(userEmail+"signin");
@@ -9,6 +14,15 @@ if(userEmail!="null")
     document.getElementById("userState").innerHTML="in";
     }else
     {
+      const users = collection(db, "user");
+      const q1=query(users,where("Email", "==",localStorage.getItem("user")));
+      const querySnapshot = await getDocs(q1);
+        if(querySnapshot.size==0)
+        {
+        signoutSite();
+        document.getElementById("userState").innerHTML="in";
+        }
+        else
         document.getElementById("userState").innerHTML="out";
     }
 
@@ -18,3 +32,23 @@ else
     console.log(userEmail +"signed out")
     document.getElementById("userState").innerHTML="In";
 }   
+
+function signoutSite()
+{
+  var user=localStorage.getItem("user");
+  if(user=="null" || user==null)
+  {
+    alert("You are not yet signed in");
+    return;
+  }
+  signOut(auth).then((e) => {
+    
+    localStorage.setItem("user","null");
+    alert("You need to complete your registration, please sign in again")
+  //window.location.replace("./index.html");
+}).catch((error) => {
+ alert("error");
+});
+}
+
+
--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -1,36 +1,46 @@
 import {auth,provider} from "./firebase_auth.js"
 import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
-
-function signIn(){
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js"; 
+import {db} from "./firestore_connect.js"
+import {  query, where,getDocs } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+ async function signIn(){
   if(localStorage.getItem("user")!="null" && localStorage.getItem("user")!=null)
   {
     alert("You are already signed in as " +localStorage.getItem("user"));
     return;
   }
-    signInWithPopup(auth, provider)
-    .then((result) => {
-
+    await signInWithPopup(auth, provider)
+    .then(async (result) => {
+      const users = collection(db, "user");
+      const q1=query(users,where("Email", "==",result.user.email));
+      const querySnapshot = await getDocs(q1);
+      
 
       
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       // The signed-in user info
+      
       localStorage.setItem("user",result.user.email);
-      window.location.replace("./index.html");
-     
-     // document.location.reload()
-     //window.location = "./recharge.html";
       const useremail = result.user.email;
 
       alert(useremail);
+      if(querySnapshot.size==0)
+        window.location.replace("./signup.html");
+      else
+        window.location.replace("./index.html");
+     
+     // document.location.reload()
+     //window.location = "./recharge.html";
+      
       // ...
     }).catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
       // The email of the user's account used.
-      const email = error.customData.email;
+     // const email = error.customData.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
